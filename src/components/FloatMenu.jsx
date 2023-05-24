cb244d63a30f22cfe23a266ccdfa7c03aea8f80b
Lucas Null <lucasnull@live.com>
2023-05-24 10:28:55 -0300

    feat: send whatsapp message and send email to

--- src/components/FloatMenu.jsx
+++ src/components/FloatMenu.jsx
@@ -3,12 +3,23 @@ import whatsappIcon from "../assets/whatsappIcon.png";
 import sendMailIcon from "../assets/sendMailIcon.png";
 
 const FloatingMenuButton = () => {
+  const whatsAppMsg = () => {
+    const number = "5519991096104";
+    const message = "Olá, vim pelo seu portfólio e gostaria de te conhecer melhor!";
+    window.open(`https://api.whatsapp.com/send?phone=${number}&text=${message}`, "_blank");
+  };
   return (
     <div className="flex flex-col fixed bottom-24 right-6 gap-3">
       <a className="w-20 h-20  bg-lightgreen rounded-full flex items-center justify-center">
-        <img src={whatsappIcon} />
+        <button onClick={whatsAppMsg}>
+          <img src={whatsappIcon} />
+        </button>
       </a>
-      <a className="w-20 h-20 bg-lightgreen rounded-full flex items-center justify-center">
+      <a
+        className="w-20 h-20 bg-lightgreen rounded-full flex items-center justify-center"
+        href="mailto:lucasnull@live.com"
+        target="_blank"
+      >
         <img src={sendMailIcon} />
       </a>
     </div>
